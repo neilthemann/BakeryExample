3f5f5d34e09e6ed7c329a59552e66a12aebc6d1d
mannn <mannn@otc.edu>
2018-11-07 12:10:55 -0600

    Added menus

--- bakery.py
+++ bakery.py
@@ -8,3 +8,27 @@ totalCupcakes = 0
 totalPies = 0
 totalMuffins = 0
 totalCakes = 0
+userExit = False
+userMenuChoice = 0
+userSubMenuExit = False
+userSubMenuChoice = 0
+
+while (userExit == False):
+    print("Welcome to the bakery!")
+    print("What would you like to do?")
+    print("1. Add an item to your cart")
+    print("2. View your cart")
+    print("3. Checkout and exit")
+    userMenuChoice = int(input())
+
+    if (userMenuChoice == 1):
+        while(userSubMenuExit == False):
+            print("What would you like to add to your cart?")
+            print("1. Cupcakes (1 dozen) - $5")
+            print("2. Apple Pie - $6")
+            print("3. Muffins (1 dozen) - $4")
+            print("4. Birthday Cake - $15")
+            print("5. Return to previous menu")
+            userSubMenuChoice = int(input())
+
+    
